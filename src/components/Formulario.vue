4d1c7f4695f61bf58ae58ba79f0a03c553f6b0b2
PsychoKillerdd <onlyvicenteuribe@gmail.com>
2025-03-01 22:31:16 -0300

    Inicio del html y integracion de clases tailwind

---
+++ src/components/Formulario.vue
@@ -0,0 +1,23 @@
+<script>
+        
+        
+        
+        
+        
+        
+        
+            export default {
+                
+            }
+</script>
+<template>
+    <div class="md:w-1/2">
+            <h2 class="font-black text-3xl text-center">Seguimiento pacientes</h2>
+            <p class="text-lg mt-5 text-center mb-10">
+                <span>anade pacientes y
+                    <span class="text-indigo-600 font-bold">administralos</span>
+                </span>
+            </p>
+    </div>
+</template>
+
